977b28090d987fe5864a037952994bf3dcbcced5
laurentyzhang <35499257+laurentyzhang@users.noreply.github.com>
2020-02-22 21:46:14 -0700

    Initial commit

---
+++ contracts/KittyAuction.sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.5.0;
+
+import "./KittyBreeding.sol";
+import "./Auction/ClockAuction.sol";
+import "./Auction/SiringClockAuction.sol";
+import "./Auction/SaleClockAuction.sol";
+
+/// @title Handles creating auctions for sale and siring of kitties.
+///  This wrapper of ReverseAuction exists only so that users can create
+///  auctions with only one transaction.
+contract KittyAuction is KittyBreeding {
+    /// @dev Put a kitty up for auction.
+    ///  Does some ownership trickery to create auctions in one tx.
+    function createSaleAuction(
+        uint256 _kittyId,
+        uint256 _startingPrice,
+        uint256 _endingPrice,
+        uint256 _duration
+    )
+        public
+        whenNotPaused
+    {
+        // Auction contract checks input sizes
+        // If kitty is already on any auction, this will throw
+        // because it will be owned by the auction contract.
+        require(_owns(msg.sender, _kittyId));
+        _approve(_kittyId, address(saleAuction));
+        // Sale auction throws if inputs are invalid and clears
+        // transfer and sire approval after escrowing the kitty.
+        saleAuction.createAuction(
+            _kittyId,
+            _startingPrice,
+            _endingPrice,
+            _duration,
+            msg.sender
+        );
+    }
+
+    /// @dev Put a kitty up for auction to be sire.
+    ///  Performs checks to ensure the kitty can be sired, then
+    ///  delegates to reverse auction.
+    function createSiringAuction(
+        uint256 _kittyId,
+        uint256 _startingPrice,
+        uint256 _endingPrice,
+        uint256 _duration
+    )
+        public
+        whenNotPaused
+    {
+        // Auction contract checks input sizes
+        // If kitty is already on any auction, this will throw
+        // because it will be owned by the auction contract.
+        require(_owns(msg.sender, _kittyId));
+        require(isReadyToBreed(_kittyId));
+        _approve(_kittyId, address(siringAuction));
+        // Siring auction throws if inputs are invalid and clears
+        // transfer and sire approval after escrowing the kitty.
+        siringAuction.createAuction(
+            _kittyId,
+            _startingPrice,
+            _endingPrice,
+            _duration,
+            msg.sender
+        );
+    }
+
+    /// @dev Completes a siring auction by bidding.
+    ///  Immediately breeds the winning matron with the sire on auction.
+    /// @param _sireId - ID of the sire on auction.
+    /// @param _matronId - ID of the matron owned by the bidder.
+    function bidOnSiringAuction(
+        uint256 _sireId,
+        uint256 _matronId
+    )
+        public
+        payable
+        whenNotPaused
+    {
+        // Auction contract checks input sizes
+        require(_owns(msg.sender, _matronId));
+        require(isReadyToBreed(_matronId));
+        require(_canBreedWithViaAuction(_matronId, _sireId));
+        uint256 currPrice = siringAuction.getCurrentPrice(_sireId);
+        uint256 bidAmount = msg.value;
+        bool doAutoBirth = false;
+
+        if (bidAmount >= currPrice + autoBirthFee) {
+            bidAmount -= autoBirthFee;
+            doAutoBirth = true;
+        }
+
+        // Siring auction will throw if the bid fails.
+        siringAuction.bid.value(bidAmount)(_sireId);
+        _breedWith(_matronId, _sireId);
+
+        if (doAutoBirth) {
+            // Auto birth fee provided, trigger autobirth event
+            bytes memory matronBytes = hashmap.getBytes("kitties", _matronId);
+            Kitty memory matron = _bytesToKitty(matronBytes);
+            emit AutoBirth(_matronId, matron.cooldownEndTime);
+        }
+    }
+
+    /// @dev Transfers the balance of the sale auction contract
+    /// to the KittyCore contract. We use two-step withdrawal to
+    /// prevent two transfer calls in the auction bid function.
+    function withdrawAuctionBalances() external onlyCOO {
+        saleAuction.withdrawBalance();
+        siringAuction.withdrawBalance();
+    }
+}
